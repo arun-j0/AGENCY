e3cb4fcffbcb9b1c68d384a3201749f02bd54d0e
arun-j0 <arun2310kumar2002@gmail.com>
2024-04-19 02:32:19 +0530

    updated favicon

--- src/components/LogInSignUpFform.jsx
+++ src/components/LogInSignUpFform.jsx
@@ -32,7 +32,7 @@ const LoginSignupForm = ({ onClose }) => {
             transition={{ duration: 0.3 }}
             className="fixed left-0 z-50 flex items-center justify-center w-full h-full top-[340px]"
         >
-            <div ref={formRef} className="relative flex flex-col items-center justify-center w-full max-w-md p-8 bg-gray-900 rounded-md shadow-md">
+            <div ref={formRef} className="relative flex flex-col items-center justify-center w-full max-w-md p-8 border-2 border-orange-300 rounded-md shadow-md bg-neutral-900/80">
                 <button onClick={onClose} className="absolute text-gray-600 top-2 right-2 hover:text-gray-800 focus:outline-none">
                     <X size={24} />
                 </button>
@@ -50,18 +50,18 @@ const LoginSignupForm = ({ onClose }) => {
 const LoginForm = ({ onToggleForm }) => {
     return (
         <>
-            <h2 className="mb-6 text-xl font-bold text-white">Login</h2>
+            <h2 className="mb-6 text-xl font-bold text-white ">Login</h2>
             <form className="w-full">
                 
                 <div className="w-full mb-4">
-                    <input type="email" placeholder="Email" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-blue-500" />
+                    <input type="email" placeholder="Email" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-orange-500" />
                 </div>
                 <div className="w-full mb-6">
-                    <input type="password" placeholder="Password" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-blue-500" />
+                    <input type="password" placeholder="Password" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-orange-500" />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button type="submit" className="px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500">Sign In</button>
-                    <button type="button" onClick={onToggleForm} className="text-blue-500 hover:underline">Don't have an account? Sign Up</button>
+                    <button type="submit" className="px-6 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-500">Sign In</button>
+                    <button type="button" onClick={onToggleForm} className="text-orange-500 hover:underline">Don't have an account? Sign Up</button>
                 </div>
             </form>
         </>
@@ -75,17 +75,17 @@ const SignupForm = ({ onToggleForm }) => {
             <form className="w-full">
                 {/* Signup form fields */}
                 <div className="w-full mb-4">
-                    <input type="text" placeholder="Name" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-green-500" />
+                    <input type="text" placeholder="Name" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-orange-500" />
                 </div>
                 <div className="w-full mb-4">
-                    <input type="email" placeholder="Email" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-green-500" />
+                    <input type="email" placeholder="Email" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-orange-500" />
                 </div>
                 <div className="w-full mb-6">
-                    <input type="password" placeholder="Password" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-green-500" />
+                    <input type="password" placeholder="Password" className="w-full px-4 py-2 text-gray-800 placeholder-gray-500 bg-gray-200 rounded-md focus:outline-none focus:ring focus:ring-orange-500" />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button type="submit" className="px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500">Sign Up</button>
-                    <button type="button" onClick={onToggleForm} className="text-blue-500 hover:underline">Already have an account? Log In</button>
+                    <button type="submit" className="px-6 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-500">Sign Up</button>
+                    <button type="button" onClick={onToggleForm} className="text-orange-500 hover:underline">Already have an account? Log In</button>
                 </div>
             </form>
         </>
